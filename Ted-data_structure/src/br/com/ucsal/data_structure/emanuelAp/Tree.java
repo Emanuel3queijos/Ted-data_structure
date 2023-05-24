cf36a0389240683be92af210300f41545ba60498
Emanuel <101946589+mano3queijos@users.noreply.github.com>
2023-05-24 15:51:27 -0300

    ted

--- Ted-data_structure/src/br/com/ucsal/data_structure/emanuelAp/Tree.java
+++ Ted-data_structure/src/br/com/ucsal/data_structure/emanuelAp/Tree.java
@@ -8,5 +8,48 @@ package br.com.ucsal.data_structure.emanuelAp;
 public class Tree {
 	public class Node {
 
+		public Node left;
+		public Node right;
+		public Integer value;
+
+		public Node(Integer value) {
+			this.value = value;
+			left = null;
+			right = null;
+		}
+
 	}
+
+	public Node root;
+
+	public Tree() {
+		root = null;
+	}
+
+	public boolean insert(Integer value) {
+
+		Node localNode = root;
+
+		Node node = new Node(value);
+
+		if (isNull()) {
+			root = node;
+			return true;
+		}
+
+		while (localNode != null) {
+
+		}
+
+		return false;
+
+	}
+
+	public boolean isNull() {
+		if (root == null) {
+			return true;
+		}
+		return false;
+	}
+
 }
