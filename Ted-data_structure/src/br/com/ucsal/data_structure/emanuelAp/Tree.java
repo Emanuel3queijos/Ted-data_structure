e9478a41cced760c92b9e3fb1b415e144ba99c43
Emanuel <101946589+mano3queijos@users.noreply.github.com>
2023-05-24 15:18:31 -0300

    ted

---
+++ Ted-data_structure/src/br/com/ucsal/data_structure/emanuelAp/Tree.java
@@ -0,0 +1,12 @@
+//******************************
+//
+//https://github.com/mano3queijos
+//
+//******************************
+package br.com.ucsal.data_structure.emanuelAp;
+
+public class Tree {
+	public class Node {
+
+	}
+}
