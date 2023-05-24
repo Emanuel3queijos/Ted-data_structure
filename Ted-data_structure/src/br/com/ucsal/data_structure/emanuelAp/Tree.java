07672b07d0fd7ab9bc35c006f118467db1a98271
Emanuel <101946589+mano3queijos@users.noreply.github.com>
2023-05-24 17:57:49 -0300

    ted

--- Ted-data_structure/src/br/com/ucsal/data_structure/emanuelAp/Tree.java
+++ Ted-data_structure/src/br/com/ucsal/data_structure/emanuelAp/Tree.java
@@ -1,13 +1,10 @@
-//******************************
-//
-//https://github.com/mano3queijos
-//
-//******************************
 package br.com.ucsal.data_structure.emanuelAp;
+
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Tree {
 	public class Node {
-
 		public Node parent;
 		public Node left;
 		public Node right;
@@ -19,7 +16,6 @@ public class Tree {
 			right = null;
 			parent = null;
 		}
-
 	}
 
 	public Node root;
@@ -29,45 +25,85 @@ public class Tree {
 	}
 
 	public String insert(Integer value) {
-
 		Node localNode = root;
-
 		Node node = new Node(value);
 
 		if (isNull()) {
 			root = node;
-			return "value added to the root";
+			return "Value added to the root";
 		}
 
 		while (localNode != null) {
 			node.parent = localNode;
 			if (value == localNode.value) {
-				return "the value already exists in the tree, not inserted";
+				return "The value already exists in the tree, not inserted";
 			} else if (value < localNode.value) {
 				localNode = localNode.left;
 				if (localNode == null) {
 					node.parent.left = node;
-					return "node added to left";
+					return "Node added to the left";
 				}
 			} else {
 				localNode = localNode.right;
-
 				if (localNode == null) {
 					node.parent.right = node;
-					return "node added to right";
+					return "Node added to the right";
 				}
 			}
-
 		}
-		return "value does not inserted";
-
+		return "Value was not inserted";
 	}
 
 	public boolean isNull() {
-		if (root == null) {
-			return true;
-		}
-		return false;
+		return root == null;
 	}
 
+	public void printTree() {
+		printTree(root);
+	}
+
+	private void printTree(Node node) {
+		if (node == null) {
+			return;
+		}
+
+		Queue<Node> queue = new LinkedList<>();
+		queue.add(node);
+
+		while (!queue.isEmpty()) {
+			int nodesAtCurrentLevel = queue.size();
+
+			while (nodesAtCurrentLevel > 0) {
+				Node currentNode = queue.poll();
+
+				if (currentNode.value == root.value) {
+					System.out.print("      " + currentNode.value + "\n" + "     // \\\\ " + "\n" + "    //   \\\\  ");
+				} else {
+					System.out.print("    " + currentNode.value + "   ");
+				}
+
+				if (currentNode.left != null)
+					queue.add(currentNode.left);
+
+				if (currentNode.right != null)
+					queue.add(currentNode.right);
+
+				nodesAtCurrentLevel--;
+			}
+
+			System.out.println();
+		}
+	}
+
+	public static void main(String[] args) {
+		Tree tree = new Tree();
+		Integer[] values = { -7, 7, 2, 1, 8, 9, 8, 7, 3, 0 };
+
+		for (int i = 0; i < values.length; i++) {
+			String inserted = tree.insert(values[i]);
+			System.out.println(inserted + " " + values[i]);
+		}
+
+		tree.printTree();
+	}
 }
